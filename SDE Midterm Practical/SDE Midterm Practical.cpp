ce4b75365e44cc98200999691c5b98a727019d7b
Allen Burton <adburton@student.fullsail.edu>
2022-06-17 23:56:20 -0700

    Add while loop

--- SDE Midterm Practical/SDE Midterm Practical.cpp
+++ SDE Midterm Practical/SDE Midterm Practical.cpp
@@ -17,25 +17,25 @@ int main()
     system("cls");
 
     string
-        q1 = "\n What gets wet while drying?\n"
+        q1 = "\n What gets wet while drying?\n\n"
         " a) A Dog\n"
         " b) A Car\n"
         " c) A Towel\n"
         " d) Sonic The Hedgehog\n"
         " \n Input your choice: ",
-        q2 = "\n What kind of band doesn't play music?\n"
+        q2 = "\n What kind of band doesn't play music?\n\n"
         " a) A Jazz Band\n"
         " b) A Metal Band\n"
         " c) A Pop Band\n"
         " d) A Rubber Band\n"
         " \n Input your choice: ",
-        q3 = " \n What has four wheels and flies?\n"
+        q3 = " \n What has four wheels and flies?\n\n"
         " a) Mosquitos\n"
         " b) Tesla Model S\n"
         " c) A Garbage Truck\n"
         " d) Hot Wheels\n"
         " \n Input your choice: ",
-        q4 = "\n What five letter word becomes shorter when you add two letters to it?\n"
+        q4 = "\n What five letter word becomes shorter when you add two letters to it?\n\n"
         " a) NASA\n"
         " b) Short\n"
         " c) Long\n"
@@ -48,67 +48,82 @@ int main()
     testQuestions.push_back(q3);
     testQuestions.push_back(q4);
 
-    srand(static_cast<unsigned  int>(time(0)));
-    std::random_shuffle(testQuestions.begin(), testQuestions.end());
-
     string tQuestion = "";
-
-    for (int i = 0; i <= 1; i++)
-    {
-        tQuestion = testQuestions[i];
-        cout << testQuestions[i];
-        i++;
-    }
-
     string choice = "";
+    bool loop = true;
 
-    cin >> choice;
+    while (loop)
+    {
 
-    if (tQuestion == q1)
-    {
-        if (choice == "c")
-        {
-            cout << "\nCORRECT! YOU WIN!\n";
-        }
-        else if (choice != "c")
-        {
-            cout << "\nWRONG ANSWER! TRY AGAIN!\n";
-        }
-    }
-    else if (tQuestion == q2)
-    {
-        if (choice == "d")
-        {
-            cout << "\nCORRECT! YOU WIN!\n";
-        }
-        else if (choice != "d")
-        {
-            cout << "\nWRONG ANSWER! TRY AGAIN!\n";
-        }
-    }
-    else if (tQuestion == q3)
-    {
-        if (choice == "c")
-        {
-            cout << "\nCORRECT! YOU WIN!\n";
-        }
-        else if (choice != "c")
-        {
-            cout << "\nWRONG ANSWER! TRY AGAIN!\n";
-        }
-    }
-    else if (tQuestion == q4)
-    {
-        if (choice == "b")
-        {
-            cout << "\nCORRECT! YOU WIN!\n";
-        }
-        else if (choice != "b")
-        {
-            cout << "\nWRONG ANSWER! TRY AGAIN!\n";
-        }
-    }
+        srand(static_cast<unsigned  int>(time(0)));
+        std::random_shuffle(testQuestions.begin(), testQuestions.end());
 
-    cin.get();
+        for (int i = 0; i <= 1; i++)
+        {
+            tQuestion = testQuestions[i];
+            cout << testQuestions[i];
+            i++;
+        }
+
+        cin >> choice;
+
+        if (tQuestion == q1)
+        {
+            if (choice == "c")
+            {
+                cout << "\nCORRECT! YOU WIN!\n";
+            }
+            else if (choice != "c")
+            {
+                cout << "\nWRONG ANSWER! TRY AGAIN!\n";
+                cin.get();
+                loop = false;
+            }
+            cin.get();
+        }
+        else if (tQuestion == q2)
+        {
+            if (choice == "d")
+            {
+                cout << "\nCORRECT! YOU WIN!\n";
+            }
+            else if (choice != "d")
+            {
+                cout << "\nWRONG ANSWER! TRY AGAIN!\n";
+                cin.get();
+                loop = false;
+            }
+            cin.get();
+        }
+        else if (tQuestion == q3)
+        {
+            if (choice == "c")
+            {
+                cout << "\nCORRECT! YOU WIN!\n";
+            }
+            else if (choice != "c")
+            {
+                cout << "\nWRONG ANSWER! TRY AGAIN!\n";
+                cin.get();
+                loop = false;
+            }
+            cin.get();
+        }
+        else if (tQuestion == q4)
+        {
+            if (choice == "b")
+            {
+                cout << "\nCORRECT! YOU WIN!\n";
+            }
+            else if (choice != "b")
+            {
+                cout << "\nWRONG ANSWER! TRY AGAIN!\n";
+                cin.get();
+                loop = false;
+            }
+            cin.get();
+        }
+        system("cls");
+    }   
 
 }
