17cb5be32155dee15a0524785a3740807a16a915
Allen Burton <adburton@student.fullsail.edu>
2022-06-17 20:16:23 -0700

    Add Multiple Choice Test

--- SDE Midterm Practical/SDE Midterm Practical.cpp
+++ SDE Midterm Practical/SDE Midterm Practical.cpp
@@ -1,10 +1,114 @@
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+using namespace std;
 
 int main()
 {
+    cout << " This is a multiple choice test\n";
+    cout << " \nInput your answer and hope for the best!\n";
+    cout << " \nPress Enter to continue... \n";
 
+    cin.get();
+    system("cls");
 
+    string
+        q1 = "\n What gets wet while drying?\n"
+        " a) A Dog\n"
+        " b) A Car\n"
+        " c) A Towel\n"
+        " d) Sonic The Hedgehog\n"
+        " \n Input your choice: ",
+        q2 = "\n What kind of band doesn't play music?\n"
+        " a) A Jazz Band\n"
+        " b) A Metal Band\n"
+        " c) A Pop Band\n"
+        " d) A Rubber Band\n"
+        " \n Input your choice: ",
+        q3 = " \n What has four wheels and flies?\n"
+        " a) Mosquitos\n"
+        " b) Tesla Model S\n"
+        " c) A Garbage Truck\n"
+        " d) Hot Wheels\n"
+        " \n Input your choice: ",
+        q4 = "\n What five letter word becomes shorter when you add two letters to it?\n"
+        " a) NASA\n"
+        " b) Short\n"
+        " c) Long\n"
+        " d) Steve\n"
+        " \n Input your choice: ";
 
-    std::cout << "This is a test!\n";
+    vector <string> testQuestions;
+    testQuestions.push_back(q1);
+    testQuestions.push_back(q2);
+    testQuestions.push_back(q3);
+    testQuestions.push_back(q4);
+
+    srand(static_cast<unsigned  int>(time(0)));
+    std::random_shuffle(testQuestions.begin(), testQuestions.end());
+
+    string tQuestion = "";
+
+    for (int i = 0; i <= 1; i++)
+    {
+        tQuestion = testQuestions[i];
+        cout << testQuestions[i];
+        i++;
+    }
+
+    string choice = "";
+
+    cin >> choice;
+
+    if (tQuestion == q1)
+    {
+        if (choice == "c")
+        {
+            cout << "\nCORRECT! YOU WIN!\n";
+        }
+        else if (choice != "c")
+        {
+            cout << "\nWRONG ANSWER! TRY AGAIN!\n";
+        }
+    }
+    else if (tQuestion == q2)
+    {
+        if (choice == "d")
+        {
+            cout << "\nCORRECT! YOU WIN!\n";
+        }
+        else if (choice != "d")
+        {
+            cout << "\nWRONG ANSWER! TRY AGAIN!\n";
+        }
+    }
+    else if (tQuestion == q3)
+    {
+        if (choice == "c")
+        {
+            cout << "\nCORRECT! YOU WIN!\n";
+        }
+        else if (choice != "c")
+        {
+            cout << "\nWRONG ANSWER! TRY AGAIN!\n";
+        }
+    }
+    else if (tQuestion == q4)
+    {
+        if (choice == "b")
+        {
+            cout << "\nCORRECT! YOU WIN!\n";
+        }
+        else if (choice != "b")
+        {
+            cout << "\nWRONG ANSWER! TRY AGAIN!\n";
+        }
+    }
+
+    cin.get();
+
 }
